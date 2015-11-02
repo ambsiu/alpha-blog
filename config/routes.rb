568e698fdc6d6783b0fdc0340a0a3d2f5954813e
amb <ambrosesiu@me.com>
2015-11-02 01:29:24 +0000

    a Create article table

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  get 'pages/index', to: 'pages#index'
-  get 'pages/about', to: 'pages#about'
+  #root 'welcome#index'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
